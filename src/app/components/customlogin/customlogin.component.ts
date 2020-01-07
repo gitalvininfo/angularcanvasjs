95d51fdefa1235f962d1b29617c5d29c770fdc7e
safetechguardians2019 <yansonalvin29@gmail.com>
2020-01-08 01:02:50 +0800

    updates landing shit

--- src/app/components/customlogin/customlogin.component.ts
+++ src/app/components/customlogin/customlogin.component.ts
@@ -6,26 +6,51 @@ declare var jQuery: any;
   styleUrls: ['./customlogin.component.css']
 })
 export class CustomloginComponent implements OnInit {
-
+  innerWidth: any;
+  showLoginForm: boolean;
+  showLanding: boolean;
   constructor() { }
 
   ngOnInit() {
-    const signUpButton = document.getElementById('signUp');
-    const ghostUpButton = document.getElementById('ghostsignUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
+    this.showLandingPage();
+  }
+  showLogin() {
+    this.showLoginForm = true;
+  }
+  goToLanding() {
+    this.showLoginForm = false;
+    this.showLanding = true;
+  }
 
-    signUpButton.addEventListener('click', () => {
-      container.classList.add("right-panel-active");
-    });
+  showLandingPage() {
+    this.innerWidth = window.innerWidth;
+    console.log(this.innerWidth);
+    if (this.innerWidth <= 600) {
+      //show the landing page if mobile
+      this.showLanding = true;
+      this.showLoginForm = false;
+    }
+    // dont show the landing page if pc
+    else {
+      this.showLanding = false;
+      this.showLoginForm = true;
+      const signUpButton = document.getElementById('signUp');
+      const ghostUpButton = document.getElementById('ghostsignUp');
+      const signInButton = document.getElementById('signIn');
+      const container = document.getElementById('container');
 
-    signInButton.addEventListener('click', () => {
-      container.classList.remove("right-panel-active");
-    });
+      signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+      });
 
-    ghostUpButton.addEventListener('click', () => {
-      container.classList.add("right-panel-active");
-    });
+      signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+      });
+
+      ghostUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+      });
+    }
   }
 
 }
