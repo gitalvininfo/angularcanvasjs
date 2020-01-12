287f904448a59a236ce1ca1c876ecf5ed3e38f60
safetechguardians2019 <yansonalvin29@gmail.com>
2020-01-12 20:11:01 +0800

    fixed responsive in mobile

--- src/app/components/customlogin/customlogin.component.ts
+++ src/app/components/customlogin/customlogin.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-declare var jQuery: any;
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+export interface UserRegisterInfo {
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  password: string;
+  c_password: string;
+}
+
 @Component({
   selector: 'app-customlogin',
   templateUrl: './customlogin.component.html',
@@ -9,48 +19,82 @@ export class CustomloginComponent implements OnInit {
   innerWidth: any;
   showLoginForm: boolean;
   showLanding: boolean;
-  constructor() { }
 
-  ngOnInit() {
-    this.showLandingPage();
+  //for registration
+  public regForm: FormGroup;
+  public emailExist: boolean;
+  usernameExists: boolean;
+  debouncer: any;
+
+
+
+  constructor() {
+
   }
+
+
   showLogin() {
-    this.showLoginForm = true;
+    this.showLanding = false;
   }
-  goToLanding() {
-    this.showLoginForm = false;
+  goToLandingPage() {
     this.showLanding = true;
   }
 
   showLandingPage() {
     this.innerWidth = window.innerWidth;
-    console.log(this.innerWidth);
-    if (this.innerWidth <= 600) {
-      //show the landing page if mobile
-      this.showLanding = true;
-      this.showLoginForm = false;
-    }
-    // dont show the landing page if pc
-    else {
-      this.showLanding = false;
-      this.showLoginForm = true;
-      const signUpButton = document.getElementById('signUp');
-      const ghostUpButton = document.getElementById('ghostsignUp');
-      const signInButton = document.getElementById('signIn');
-      const container = document.getElementById('container');
 
-      signUpButton.addEventListener('click', () => {
-        container.classList.add("right-panel-active");
-      });
+    //web switch
+    const loginWeb = document.getElementById('loginWeb');
+    const createAccountWeb = document.getElementById('createAccountWeb');
+    const signUpWeb = document.getElementById('signUpWeb');
 
-      signInButton.addEventListener('click', () => {
+    //mobile switch
+    const createAccountMobile = document.getElementById('createAccountMobile');
+    const alreadyhaveaccountMobile = document.getElementById('alreadyhaveaccountMobile');
+    const getStartedMobile = document.getElementById('getStartedMobile');
+
+    const container = document.getElementById('container');
+    container.classList.add("right-panel-active");
+
+    loginWeb.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+
+    signUpWeb.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
+
+    createAccountWeb.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
+
+
+    createAccountMobile.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
+
+    if (getStartedMobile) {
+      getStartedMobile.addEventListener('click', () => {
         container.classList.remove("right-panel-active");
       });
-
-      ghostUpButton.addEventListener('click', () => {
-        container.classList.add("right-panel-active");
-      });
     }
+
+    alreadyhaveaccountMobile.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+
+    if (this.innerWidth <= 600) {
+      this.showLanding = true;
+    }
+    else {
+      this.showLanding = false;
+
+    }
+  }
+
+  ngOnInit() {
+    this.showLandingPage();
+
   }
 
 }
